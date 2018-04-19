c56aea90246e531343354dcee9051fa304bc91f7
ivnsantos <ivansantos.ivn@gmail.com>
2018-04-19 14:38:41 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,37 +1,6 @@
-// var app = require('./server/config/express');
-// var http = require('http');
-
-
-// http.createServer(app)
-// .listen(process.env.PORT || 3000, function() {
-// 	console.log('Servidor iniciado na porta 3000');
-// });
-
-
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-//var privateKey  = fs.readFileSync('./domain-key.txt', 'utf8');//
-//var certificate = fs.readFileSync('./domain-crt.txt', 'utf8');
-
-//var credentials = {key: privateKey, cert: certificate};
 var app = require('./server/config/express');
 
-function ensureSecure(req, res, next){
-  if(req.secure){
-    return next();
-  };
-  res.redirect('https://' + req.hostname + req.url);
-};
-
-app.all('*', ensureSecure);
-
-//app.get('/', function(req, res){
- // res.send('<h2>Bugph rodando com SSL</h2>');
-//})
-
-var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(80);
-//httpsServer.listen(443);
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
